accc251e75f7dbc00de3dfca85e4f410dd3426c4
DominikTroendle <domi.troendle@gmail.com>
2025-05-21 14:51:43 +0200

    added simple drawImage function

--- scripts/game.js
+++ scripts/game.js
@@ -1,7 +1,13 @@
 let canvas;
+let ctx;
 let character = new Image();
 
 function init() {
     canvas = document.getElementById('canvas');
-    character.src = 'img/characters/Character02/Idle/All Characters-Character02-Idle_00';
+    ctx = canvas.getContext('2d');
+    character.src = '../img/characters/Character02/Idle/All Characters-Character02-Idle_00.png';
+
+    setTimeout(function () {
+        ctx.drawImage(character, 20, 20, 400, 300);
+    }, 100);
 }
