bbcac8829dead2499fec74d8f6d888784830c842
DominikTroendle <domi.troendle@gmail.com>
2025-05-21 15:36:14 +0200

    added character and enemy classes with basic structure

---
+++ classes/character.class.js
@@ -0,0 +1,16 @@
+class Character extends MovableObject {
+    x;
+    y;
+
+    constructor () {
+        super();
+    }
+
+    moveRight() {
+
+    }
+
+    jump() {
+
+    }
+}
---
+++ classes/enemy.class.js
@@ -0,0 +1,12 @@
+class Enemy extends MovableObject{
+    x;
+    y;
+
+    constructor () {
+        super();
+    }
+
+    moveLeft() {
+
+    }
+}
