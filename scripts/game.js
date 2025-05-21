aec7c59d293ebd21db6e2a128e611de0d767acfb
DominikTroendle <domi.troendle@gmail.com>
2025-05-21 13:53:21 +0200

    added sprite files

---
+++ scripts/game.js
@@ -0,0 +1,7 @@
+let canvas;
+let character = new Image();
+
+function init() {
+    canvas = document.getElementById('canvas');
+    character.src = 'img/characters/Character02/Idle/All Characters-Character02-Idle_00';
+}
