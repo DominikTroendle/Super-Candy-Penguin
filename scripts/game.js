b3fc1ca67a57ce516e296a3aea125d2a387b6e22
DominikTroendle <domi.troendle@gmail.com>
2025-05-21 15:01:07 +0200

    added class MovableObject with basic structure

--- scripts/game.js
+++ scripts/game.js
@@ -1,13 +1,8 @@
 let canvas;
 let ctx;
-let character = new Image();
+let character = new MovableObject();
 
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
-    character.src = '../img/characters/Character02/Idle/All Characters-Character02-Idle_00.png';
-
-    setTimeout(function () {
-        ctx.drawImage(character, 20, 20, 400, 300);
-    }, 100);
 }
