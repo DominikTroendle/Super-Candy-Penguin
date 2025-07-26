6621171b7ed4f2e5d979ccc1335ffde57d688c99
DominikTroendle <domi.troendle@gmail.com>
2025-07-26 12:26:09 +0200

    added penguin animation on start screen when play button is pressed

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function startGame() {
     document.getElementById('penguin-animated').classList.remove('penguin-animation-idle');
-    document.getElementById('penguin-animated').classList.remove('penguin-animation-jump');
+    document.getElementById('penguin-animated').style.backgroundImage = "url('./img/menu-screens/sprite-sheets/penguin-jump-sprite-sheet.png')";
+    document.getElementById('start-screen').style.gap = '11.5px';
+    document.getElementById('penguin-animated').classList.add('penguin-animation-jump');
     setTimeout(() => {
         document.getElementById('overlays').classList.add('d-none');
         document.getElementById('penguin-animated').classList.remove('penguin-animation-jump');
